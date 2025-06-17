9d5699511a5d4dde99e865840586c31867820627
Anush Jain <120271799+sike990@users.noreply.github.com>
2025-06-17 13:20:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (45.51%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -11,23 +11,21 @@
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-        if(root == nullptr)return 0;
-        stack<pair<TreeNode* , int >> st;
-        st.push({root , 1});
-        int ans = 0;
-        while(!st.empty()){
+    void solve(TreeNode* root , int &ans , int lvl ){
 
-            pair<TreeNode* , int> myp = st.top();
-            st.pop();
-            int lvl = myp.second;
-            ans = max(ans , lvl);
-            TreeNode* node = myp.first;
-            if(node -> left != nullptr)st.push({node -> left , lvl + 1});
-            if(node -> right != nullptr)st.push({node -> right , lvl + 1});
-            
+        if(root == nullptr){
+            ans = max(lvl , ans);
+            return; 
         }
 
+        solve(root -> left , ans , lvl+1);
+        solve(root -> right , ans , lvl + 1);
+
+    }
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr)return 0;
+        int ans = 0;
+        solve(root , ans , 0);
         return ans;
     }
 };
