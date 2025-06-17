0f9ddd16ff9b7274ca356ff9c0c346a86237cd96
Anush Jain <120271799+sike990@users.noreply.github.com>
2025-06-17 13:07:16 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (97.39%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -11,18 +11,32 @@
  */
 class Solution {
 public:
-    void solve(TreeNode * root , vector<vector<int>> &ans , int lvl){
+    // void solve(TreeNode * root , vector<vector<int>> &ans , int lvl){
 
-        if(root == nullptr)return;
-        if(ans.size() <= lvl)ans.resize(lvl+1);
-        ans[lvl].push_back(root -> val);
+    //     if(root == nullptr)return;
+    //     if(ans.size() <= lvl)ans.resize(lvl+1);
+    //     ans[lvl].push_back(root -> val);
 
-        solve(root -> left , ans , lvl + 1);
-        solve(root -> right , ans , lvl + 1);
-    }
+    //     solve(root -> left , ans , lvl + 1);
+    //     solve(root -> right , ans , lvl + 1);
+    // }
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> ans;
-        solve(root , ans , 0);
+        if(root == nullptr)return {};
+        //solve(root , ans , 0);
+        stack<pair<TreeNode* , int>> st;
+        st.push({root , 0});
+        while(!st.empty()){
+             pair<TreeNode* , int> myp = st.top();
+             st.pop();
+             int lvl = myp.second;
+             TreeNode* node = myp.first;
+             if(ans.size() <= lvl)ans.resize(lvl+1);
+             ans[lvl].push_back(node -> val);
+             if(node -> right != nullptr)st.push({node -> right , lvl + 1});
+             if(node -> left != nullptr)st.push({node -> left , lvl + 1});
+             
+        }
         return ans;
     }
 };
