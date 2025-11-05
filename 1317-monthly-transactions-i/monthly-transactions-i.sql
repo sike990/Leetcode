b1cbfd50e7aa826589b108686b696f91238de7d8
Anush Jain <120271799+sike990@users.noreply.github.com>
2025-11-05 10:36:31 +0530

    Time: 559 ms (59.75%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,5 +1,22 @@
 # Write your MySQL query statement below
-select  concat(YEAR(trans_date) , "-", case when month(trans_date) < 10 then concat('0',month(trans_date)) else month(trans_date) end) as month , country , count(*) as trans_count , sum(case  when state = "approved" then 1 else 0 end) as approved_count ,
-sum(amount) as trans_total_amount, sum(case when state= "approved" then amount else 0 end) as approved_total_amount
+select concat(year(trans_date) , "-" , 
+case
+when length(month(trans_date)) = 1 then concat('0' , month(trans_date))
+else month(trans_date)
+end
+)
+as month , country , count(id) as trans_count , sum(
+    case
+    when state = "approved" then 1
+    else 0
+    end
+) as approved_count , 
+sum(amount) as trans_total_amount , 
+sum(
+   case
+    when state = "approved" then amount
+    else 0
+    end
+) as approved_total_amount
 from Transactions
-group by concat(YEAR(trans_date) , "-", month(trans_date)) ,country;
+group by month , country;
