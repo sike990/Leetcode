42af55098e0d4f9402580edcbeb04f28d6432309
Anush Jain <120271799+sike990@users.noreply.github.com>
2025-11-08 10:31:28 +0530

    Time: 23 ms (5.85%) | Memory: 23.2 MB (53.75%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,23 +1,6 @@
 class Solution {
 public:
-    int findit(int &x , int &y){
-        if(x >= 0 && x <= 2){
-            if(y >= 0 && y <= 2)return 0;
-            if(y >= 3 && y <= 5)return 1;
-            if(y >= 6 && y <= 8)return 2;
-        }
-        if(x >= 3 && x <= 5){
-            if(y >= 0 && y <= 2)return 3;
-            if(y >= 3 && y <= 5)return 4;
-            if(y >= 6 && y <= 8)return 5;
-        }
-        if(x >= 6 && x <= 8){
-            if(y >= 0 && y <= 2)return 6;
-            if(y >= 3 && y <= 5)return 7;
-            if(y >= 6 && y <= 8)return 8;
-        }
-        return 0;
-    }
+
     bool isValidSudoku(vector<vector<char>>& board) {
         int n = board.size() , m = board[0].size();
         //setting up for column
@@ -32,8 +15,8 @@ public:
             for(int j = 0 ; j < 9 ; j++){
                 char ch = board[i][j];
                 if(ch == '.')continue;
-                int seci = findit(i,j);
-                //cout << ch <<" " << int(ch - '1') << endl;
+                int seci = ((3)*(i/3))+(j/3);
+                cout << i << j <<" "<< seci << endl;
                 bool ncol = !col[i][ch - '1'];
                 bool nrow = !row[j][ch-'1'];
                 bool nsec = !sec[seci][ch - '1'];
