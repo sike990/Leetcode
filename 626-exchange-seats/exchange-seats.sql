4c3aafe85d2778fedd6d44f93ee20aca90bed388
Anush Jain <120271799+sike990@users.noreply.github.com>
2025-12-03 15:01:29 +0530

    Time: 302 ms (90.29%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,12 +1,9 @@
 # Write your MySQL query statement below
-select s1.id,
-case
-when s1.id%2 = 0 then s3.student
-when s1.id%2 <> 0 and s2.student is not null then s2.student
-else s1.student
-end as student
-from Seat s1 
-left join Seat s2
-on s1.id = s2.id-1 
-left join Seat s3
-on s1.id-1 = s3.id
+select case
+when id%2=1 and id+1 in (select id from Seat) then id+1
+when id%2 = 0 then id-1
+else id
+end as id,
+student
+from Seat
+order by id
